779f5edbb1086b43b64c667932fdfde9653532d9
Wonkook Lee <wonkook.lee@medistream.co.kr>
2022-06-30 00:40:49 +0900

    초기  세팅

---
+++ src/components/CompA.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h2>{{ data.id }}</h2>
+    <p>{{ data.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompA',
+  data () {
+    return {
+      data: {}
+    }
+  },
+  mounted () {
+    this.fetching()
+  },
+  methods: {
+    fetching () {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(res => res.json())
+      .then(json => this.data = json)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: dodgerblue;
+  height: 600px;
+
+  h2 {
+    margin: 0;
+    color: #fff;
+    font-size: 60px;
+  }
+
+  p {
+    color: #fff;
+    font-size: 24px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <button @click="changeIndex">CNT</button>
+    <div class="home">
+      <div>
+        <keep-alive :include="cacheList">
+          <component :is="changeComponent()"></component>
+        </keep-alive>
+      </div>
+      <div>
+        <keep-alive max="3">
+          <component :is="'comp-b'"></component>
+        </keep-alive>
+      </div>
+      <div>
+        <keep-alive max="3">
+          <component :is="'comp-c'"></component>
+        </keep-alive>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CompA from '@/components/CompA.vue'
+import CompB from '@/components/CompB.vue'
+import CompC from '@/components/CompC.vue'
+import CompD from '@/components/CompD.vue'
+import CompE from '@/components/CompE.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CompA,
+    CompB,
+    CompC,
+    CompD,
+    CompE
+  },
+  data () {
+    return {
+      cacheList: [
+        'CompA',
+        'CompB',
+        'CompC',
+        'CompD',
+        'CompE'
+      ],
+      currentPage: 'comp-a',
+      pages: ['comp-a', 'comp-b', 'comp-c', 'comp-d', 'comp-e'],
+      index: 0
+    }
+  },
+  created () {
+    // window.addEventListener('keypress', (e) => {
+    //   e.preventDefault()
+    //   this.changeComponent
+    // })
+  },
+  methods: {
+    changeComponent () {
+      return this.pages[this.index]
+    },
+    changeIndex () {
+      if (this.index === 4) {
+        this.index = 0
+        return
+      }
+      this.index = this.index + 1
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  width: 300%;
+
+  & div {
+    width: 100%;
+  }
+
+  & div >>> div {
+    width: 100%;
+  }
+}
+</style>
